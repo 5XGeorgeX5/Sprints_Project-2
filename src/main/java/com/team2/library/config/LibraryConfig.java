015a889042fcbbd729dd755e51b327591ad42b47
NoureldinAbdelrahman <noureldin.gamaleldin@gmail.com>
2025-08-01 05:06:05 +0300

    Created a POJO class to allow the use of the externalized configurations

---
+++ src/main/java/com/team2/library/config/LibraryConfig.java
@@ -0,0 +1,36 @@
+package com.team2.library.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "library")
+public class LibraryConfig {
+
+    private String name;
+    private String mode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryConfig{" +
+                "name='" + name + '\'' +
+                ", mode='" + mode + '\'' +
+                '}';
+    }
+}
